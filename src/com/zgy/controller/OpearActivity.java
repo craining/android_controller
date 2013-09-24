4ad86e478de53bf8d99f707f588e6a320f76f4f8
craining <craining@163.com>
2013-09-24 16:01:21 +0800

    lib

---
+++ src/com/zgy/controller/OpearActivity.java
@@ -0,0 +1,25 @@
+package com.zgy.controller;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class OpearActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
+
+}
